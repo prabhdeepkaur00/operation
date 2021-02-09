603d1ebe7c6e18b8f44bd25224a30e8b6edf61bf
Prabhdeep kaur <75611353+prabhdeepkaur00@users.noreply.github.com>
2021-02-09 19:58:45 +0530

    practical-6

---
+++ main.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int sum=26,n=10;
+    float avg;
+    avg=sum/n;
+    printf("\nimplicit type casting:%f",avg);
+  
+    return 0;
+}
+
+
+
