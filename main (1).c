8e95e8e0a2dc875afded3ee8313eaac49367704a
Prabhdeep kaur <75611353+prabhdeepkaur00@users.noreply.github.com>
2021-02-09 20:22:34 +0530

    7 parctical

---
+++ main (1).c
@@ -0,0 +1,25 @@
+//WRITE A C PROGRAM TO FIND THAT THE ACCEPTEDNUMBER IS NEGATIVE OR POSITIVE OR ZERO
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int a;
+    printf("enter a:");
+    scanf("%d",&a);
+    if(a>=0)
+       printf("%d is a positive",a);
+    else 
+       printf("%d is a negative",a);
+    
+    
+   
+  
+    return 0;
+}
+
+
+
+
+
