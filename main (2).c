023fb41864cdfda56668a56a976ad877d986b489
Prabhdeep kaur <75611353+prabhdeepkaur00@users.noreply.github.com>
2021-02-10 22:17:48 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,20 @@
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int i,num,sum=0;
+    printf("Input any number:\n");
+    scanf("%d",&num);
+    printf("1 + ");
+    for(i=2;i<=num;i++);
+    printf("1/%d",i);
+    for(i=1;i<=num;i++)
+       sum=sum+i;
+     printf("1/%d",num);
+     printf("\nSum=1/%d",sum+1/num);
+    return 0;
+}
+
+    
