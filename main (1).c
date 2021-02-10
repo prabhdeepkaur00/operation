e05784cf281859564286c13535e8b57f579714af
Prabhdeep kaur <75611353+prabhdeepkaur00@users.noreply.github.com>
2021-02-10 19:56:34 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,22 @@
+//write a program to check whether the enetered character is capital, samll letter,digit or any special character using c programming
+
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    printf("enter any character or digit");
+    scanf("%c",&a);
+    if((a>='a'&& a<='z')||(a>='A'&&a<='Z'))
+    {
+        printf("%c is a alphabet",a);
+    }else if(a>='0'&&a<='9')
+    {
+        printf("%c is a digit",a);
+    }else
+    {
+        printf("%c is a special character",a);
+    }
+    return 0;
+}
+
