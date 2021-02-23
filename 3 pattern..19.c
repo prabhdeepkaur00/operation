558e30b78f2164461dd19572cc5004e5d28b27b6
Prabhdeep kaur <75611353+prabhdeepkaur00@users.noreply.github.com>
2021-02-23 22:28:20 +0530

    19 practical

---
+++ 3 pattern..19.c
@@ -0,0 +1,23 @@
+//W.A.P TO PRINT PATTERNS: *****
+//                         ****
+//                         ***
+//                         ** 
+//                         * 
+#include <stdio.h>
+
+int main()
+{
+    int i,j,r;
+    printf("Enter no. of rows:");
+    scanf("%d",&r);
+    for(i=r;i>=1;--i)
+    {
+        for(j=1;j<=i;++j)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
